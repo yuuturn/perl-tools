58c8493809b340be112c631b8c9fab99f77a954d
yuta fujii <innerflight0422@gmail.com>
2018-06-20 18:27:05 +0900

    no use for "DateTime::Format::ISO8601"

--- maillog_dateformat_conv.pl
+++ maillog_dateformat_conv.pl
@@ -1,12 +1,22 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use DateTime::Format::ISO8601;
+use DateTime;
+#use DateTime::Format::ISO8601;
 
 while(<>){
-  if(/(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+09:00) (.+)/){
-    my $dt = DateTime::Format::ISO8601->parse_datetime($1);
+  if(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})\+09:00 (.+)/){
+    #my $dt = DateTime::Format::ISO8601->parse_datetime($1);
+    my $dt = DateTime->new(
+      year       => $1,
+      month      => $2,
+      day        => $3,
+      hour       => $4,
+      minute     => $5,
+      second     => $6,
+      #time_zone  => 'Asia/Tokyo',
+    );
     my $out = $dt->strftime("%b %d %H:%M:%S");
-    print "$out $2\n";
+    print "$out $7\n";
   }
 }
